882bd3ba127eb2ed0aca43f90274ef962b1aef59
naman-choudhary <123643589+naman-choudhary@users.noreply.github.com>
2023-06-21 13:49:03 +0530

    Update CarListing.jsx
    Now the data of the cars will be fetched from the backend.

--- src/pages/CarListing.jsx
+++ src/pages/CarListing.jsx
@@ -1,40 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Container, Row, Col } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
 import CarItem from "../components/UI/CarItem";
-import carData from "../assets/data/carData";
 
 const CarListing = () => {
-  return (
-    <Helmet title="Cars">
-      <CommonSection title="Car Listing" />
+	const url = "http://localhost:8080/cars";
 
-      <section>
-        <Container>
-          <Row>
-            <Col lg="12">
-              <div className=" d-flex align-items-center gap-3 mb-5">
-                <span className=" d-flex align-items-center gap-2">
-                  <i class="ri-sort-asc"></i> Sort By
-                </span>
+	const [carData, setCarData] = useState([]);
+	const [selectedSortOption, setSelectedSortOption] = useState("");
+	const [sortedCarData, setSortedCarData] = useState([]);
 
-                <select>
-                  <option>Select</option>
-                  <option value="low">Low to High</option>
-                  <option value="high">High to Low</option>
-                </select>
-              </div>
-            </Col>
+	const fetchCars = async () => {
+		try {
+			const response = await axios.get(url);
+			setCarData(response.data);
+			setSortedCarData(response.data);
+		} catch (error) {
+			console.error("Error fetching car data:", error);
+		}
+	};
 
-            {carData.map((item) => (
-              <CarItem item={item} key={item.id} />
-            ))}
-          </Row>
-        </Container>
-      </section>
-    </Helmet>
-  );
+	useEffect(() => {
+		fetchCars();
+	}, []);
+
+	if (!sortedCarData) {
+		return <div>Loading...</div>;
+	}
+
+	const handleSortChange = (event) => {
+		const sortOption = event.target.value;
+		setSelectedSortOption(sortOption);
+
+		let sortedData = [...carData];
+
+		if (sortOption === "low") {
+			sortedData.sort((a, b) => a.price - b.price);
+		} else if (sortOption === "high") {
+			sortedData.sort((a, b) => b.price - a.price);
+		}
+
+		setSortedCarData(sortedData);
+	};
+
+	return (
+		<Helmet title="Cars">
+			<CommonSection title="Car Listing" />
+
+			<section>
+				<Container>
+					<Row>
+						<Col lg="12">
+							<div className=" d-flex align-items-center gap-3 mb-5">
+								<span className=" d-flex align-items-center gap-2">
+									<i class="ri-sort-asc"></i> Sort By
+								</span>
+
+								<select value={selectedSortOption} onChange={handleSortChange}>
+									<option>Select</option>
+									<option value="low">Low to High</option>
+									<option value="high">High to Low</option>
+								</select>
+							</div>
+						</Col>
+
+						{sortedCarData.map((item) => (
+							<CarItem item={item} key={item.id} />
+						))}
+					</Row>
+				</Container>
+			</section>
+		</Helmet>
+	);
 };
 
 export default CarListing;
